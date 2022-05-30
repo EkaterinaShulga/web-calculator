94011e0c5fd15da922b9d750c5ec0943e51a3750
Екатерина <shulga23@gmail.com>
2022-05-30 13:13:15 +0300

    задание по теме "Введение в Maven и Spring Boot" выполнила

---
+++ src/main/java/pro/sky/java/course2/webcalculator/CalculatorService.java
@@ -0,0 +1,15 @@
+package pro.sky.java.course2.webcalculator;
+
+public interface CalculatorService {
+
+    String showHelloCalculator();
+
+    String returnAmount(Double num1, Double num2);
+
+    String returnDifference(Double num1, Double num2);
+
+    String returnMultiplication(Double num1, Double num2);
+
+    String returnDivision(Double num1, Double num2);
+
+}
